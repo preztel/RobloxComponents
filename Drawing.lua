7ff0da65034b9cbef578a20948a7bd2b493a41d6
prezt <60546504+preztel@users.noreply.github.com>
2023-04-20 02:13:30 +1300

    Create Drawing.lua

---
+++ Drawing.lua
@@ -0,0 +1,96 @@
+local Library = {}
+
+local CreateRenderObject = Drawing.new
+local SetRenderProperty = setrenderproperty
+local GetRenderProperty = getrenderproperty
+local DestroyRenderObject = Drawing.Destroy
+
+local UserInput = game:GetService("UserInputService")
+
+function Library.OnMouse(Instance)
+    local Mouse = UserInput:GetMouseLocation()
+    
+    local InstanceVisible = GetRenderProperty(Instance, "Visible")
+    local InstancePosition = GetRenderProperty(Instance, "Position")
+    local InstanceSize = GetRenderProperty(Instance, "Size")
+
+    if InstanceVisible and (Mouse.X > InstancePosition.X) and (Mouse.X < InstancePosition.X + InstanceSize.X) and (Mouse.Y > InstancePosition.Y) and (Mouse.Y < InstancePosition.Y + InstanceSize.Y) then
+        return true
+    end
+end
+
+function Library.new(InstanceType, InstanceProperties, InstanceParent)
+    local Utility = {}
+    local Instance = CreateRenderObject(InstanceType)
+    SetRenderProperty(Instance, "Visible", true)
+    Utility.Instance = Instance
+
+    if InstanceType == "Square" then
+        SetRenderProperty(Instance, "Filled", true)
+        SetRenderProperty(Instance, "Thickness", 0)
+    elseif InstanceType == "Text" then
+        SetRenderProperty(Instance, "Size", 13)
+        SetRenderProperty(Instance, "Font", 1)
+        SetRenderProperty(Instance, "Color", Color3.fromRGB(255, 255, 255))
+    end
+
+    if Instance then
+        for Name, Value in next, InstanceProperties do
+            SetRenderProperty(Instance, tostring(Name), Value)
+        end
+    end
+
+    Utility.Mouse1Down = function(Callback)
+        UserInput.InputBegan:Connect(function(Input)
+            if Input.UserInputType == Enum.UserInputType.MouseButton1 and Library.OnMouse(Instance) then
+                Callback()
+            end
+        end)
+    end
+
+    Utility.Mouse2Down = function(Callback)
+        UserInput.InputBegan:Connect(function(Input)
+            if Input.UserInputType == Enum.UserInputType.MouseButton2 and Library.OnMouse(Instance) then
+                Callback()
+            end
+        end)
+    end
+
+    Utility.Edit = function(Name, Value)
+        SetRenderProperty(Instance, tostring(Name), Value)
+    end
+
+    if InstanceParent then
+        InstanceParent[#InstanceParent + 1] = Instance
+    end
+
+    return Utility
+end
+
+function Library:Size(xScale, xOffset, yScale, yOffset, Parent)
+    if Parent then
+        local X = xScale * GetRenderProperty(Parent, "Size").X + xOffset
+        local Y = yScale * GetRenderProperty(Parent, "Size").Y + yOffset
+        return Vector2.new(X, Y)
+    else
+        local vx, vy = workspace.CurrentCamera.ViewportSize.X, workspace.CurrentCamera.ViewportSize.Y
+        local X = xScale * vx + xOffset
+        local Y = yScale * vy + yOffset
+        return Vector2.new(X, Y)
+    end
+end
+
+function Library:Position(xScale, xOffset, yScale, yOffset, Parent)
+    if Parent then
+        local X = GetRenderProperty(Parent, "Position").X + xScale * GetRenderProperty(Parent, "Size").X + xOffset
+        local Y = GetRenderProperty(Parent, "Position").Y + yScale * GetRenderProperty(Parent, "Size").Y + yOffset
+        return Vector2.new(X, Y)
+    else
+        local vx, vy = workspace.CurrentCamera.ViewportSize.x, workspace.CurrentCamera.ViewportSize.y
+        local X = xScale * vx + xOffset
+        local Y = yScale * vy + yOffset
+        return Vector2.new(X, Y)
+    end
+end
+
+return Library
